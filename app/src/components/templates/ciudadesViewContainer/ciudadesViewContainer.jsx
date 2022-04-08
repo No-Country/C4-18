9d421d732f28984c0ba402cb143cfc1656b2ac0b
Lucía <luciafuentes2000@gmail.com>
2022-04-08 12:09:01 -0300

    nueva vista de ciudades

---
+++ app/src/components/templates/ciudadesViewContainer/ciudadesViewContainer.jsx
@@ -0,0 +1,26 @@
+import "./ciudadesViewContainer.scss"
+export const CiudadesViewContainer = () => {
+
+    return (
+        <div className="ciudadesContainer">
+        <h2>Nos encontramos en: </h2>
+        <hr />
+        
+        <div className = "cards">
+        <div className="lugares" id="card1">
+        <p>London</p>
+        </div>  
+        
+        <div classNameName="lugares" id="card2">
+        <p>Miami</p>
+        </div> 
+         
+        <div className="lugares" id="card3">
+        <p>Cancún</p>
+        </div> 
+        
+        </div>
+        </div>
+        )
+
+}
